79da55755649dc03ef00b64f5b449e1387ed1984
Carlos Tomala Reyes <cbtomala@espol.edu.ec>
2019-11-03 03:17:32 -0500

    se crea la clase video para tener un mejor manejo del video

--- app/src/main/java/com/marvel/estudiante/taller2/Video.java
+++ app/src/main/java/com/marvel/estudiante/taller2/Video.java
@@ -1,7 +1,10 @@
 package com.marvel.estudiante.taller2;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Video extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class Video extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        VideoView videoView;
+        videoView = (VideoView) findViewById(R.id.videoView);
+        MediaController mControl = new MediaController(this);
+        mControl.setAnchorView(videoView);
+        Uri uri = Uri.parse("rtsp://r3---sn-vgqskn76.googlevideo.com/Cj0LENy73wIaNAkre0JvuArGORMYDSANFC0Pdr5dMOCoAUIASARg7rvWtpbrnpVdigELRG1QSU5TQzNJNjQM/E3F12E3CC0B6568559774F4BDE0854D5E37936D5.9C4B8CA342645B536807830E82D22E3D0F7E3813/yt8/1/video.3gp");
+        videoView.setMediaController(mControl);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+        
     }
 
 }
